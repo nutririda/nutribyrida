6acfc7840cf8019e8a6a0dfdc4603bdd048edda2
nutririda <aliwaqaremail1@gmail.com>
2026-07-04 20:08:21 +0500

    Updates

--- assets/services-k7vhDdvr.js
+++ assets/services-k7vhDdvr.js
@@ -1,4 +1,4 @@
-import{o as e,t}from"./footer-Dp9JmWD1.js";function n(){return`
+import{o as e,t}from"./footer-5xFcM9cR.js";function n(){return`
 <section class="services-hero">
 
     <div class="container">
