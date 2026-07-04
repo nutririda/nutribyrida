aa6efeca9dc73611be55ff27d681f58690fb0b34
nutririda <aliwaqaremail1@gmail.com>
2026-07-04 20:00:47 +0500

    Updates

--- assets/services-nJacOH7k.js
+++ assets/services-nJacOH7k.js
@@ -1,4 +1,4 @@
-import{o as e,t}from"./footer-Baq36xRm.js";function n(){return`
+import{o as e,t}from"./footer-S8Kt124N.js";function n(){return`
 <section class="services-hero">
 
     <div class="container">
